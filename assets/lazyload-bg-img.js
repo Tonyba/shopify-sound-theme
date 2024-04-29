85219da13bb4e9f143a2077f91d5beac202aa1a6
Tonyba <anthonymessagesab@gmail.com>
2024-04-29 10:56:27 -0400

    home & product designs done

---
+++ assets/lazyload-bg-img.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyLoadImages = document.querySelectorAll(".lazyload-bg");
+
+    var lazyLoad = function() {
+        lazyLoadImages.forEach(function(image) {
+            if (image.offsetTop < window.innerHeight + window.scrollY) {
+                var src = image.getAttribute('data-src');
+                if (src) {
+                    image.style.backgroundImage = "url('" + src + "')";
+                    image.removeAttribute('data-src');
+                }
+            }
+        });
+    };
+
+    lazyLoad();
+
+    window.addEventListener("scroll", lazyLoad);
+});
