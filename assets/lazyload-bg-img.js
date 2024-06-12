e5b82c410cbd76793a4acbf9cb11b131c3351c79
Tony <37878200+Tonyba@users.noreply.github.com>
2024-06-12 09:22:36 -0400

    improvement in lazy loaded bg img

--- assets/lazyload-bg-img.js
+++ assets/lazyload-bg-img.js
@@ -1,19 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyLoadImages = document.querySelectorAll(".lazyload-bg");
-
-    var lazyLoad = function() {
-        lazyLoadImages.forEach(function(image) {
-            if (image.offsetTop < window.innerHeight + window.scrollY) {
-                var src = image.getAttribute('data-src');
-                if (src) {
-                    image.style.backgroundImage = "url('" + src + "')";
-                    image.removeAttribute('data-src');
-                }
-            }
+if ('IntersectionObserver' in window) {
+    document.addEventListener("DOMContentLoaded", function() {
+  
+      function handleIntersection(entries) {
+        entries.map((entry) => {
+          if (entry.isIntersecting) {
+            // Item has crossed our observation
+            // threshold - load src from data-src
+            entry.target.style.backgroundImage = "url('"+entry.target.dataset.src+"')";
+            // Job done for this item - no need to watch it!
+            observer.unobserve(entry.target);
+          }
         });
-    };
-
-    lazyLoad();
-
-    window.addEventListener("scroll", lazyLoad);
-});
+      }
+  
+      const headers = document.querySelectorAll('.lazyload-bg');
+      const observer = new IntersectionObserver(
+        handleIntersection,
+        { rootMargin: "100px" }
+      );
+      headers.forEach(header => observer.observe(header));
+    });
+  } else {
+    // No interaction support? Load all background images automatically
+    const headers = document.querySelectorAll('.lazyload-bg');
+    headers.forEach(header => {
+      header.style.backgroundImage = "url('"+header.dataset.src+"')";
+    });
+  }
